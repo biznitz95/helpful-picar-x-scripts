30e7277a2f8ebe9f17712c47970fcd82bea83f22
bizet <bizet@apple.com>
2022-12-13 00:03:26 -0800

    Adding commands that should be helpful

---
+++ run_this_on_boot.sh
@@ -0,0 +1,4 @@
+# Sets the display and permissions ot be able to display the camera stuff
+# Run this every time you boot up the rasberry pi and want to see the camera
+export DISPLAY=:0
+xhost +
--- setup.sh
+++ setup.sh
@@ -1,1 +1,35 @@
-Helpful script for setting up Picar-X
+# Update your system
+echo "Updating the OS system";
+sudo apt update;
+sudo apt upgrade;
+
+# Download an install robot hat
+echo "Installing robot hat";
+cd /home/
+mkdir pi
+cd /home/pi/
+git clone https://github.com/sunfounder/robot-hat.git
+cd robot-hat
+sudo python3 setup.py install
+
+# Download and install vilib
+echo "Instaling vilib"
+cd /home/pi/
+git clone https://github.com/sunfounder/vilib.git
+cd vilib
+sudo python3 install.py
+
+# Download and install picar-x
+echo "Installing picar-x"
+cd /home/pi/
+git clone -b v2.0 https://github.com/sunfounder/picar-x.git
+cd picar-x
+sudo python3 setup.py install
+
+# Install i2samp
+echo "Installing i2samp.sh"
+cd /home/pi/picar-x
+sudo bash i2samp.sh
+
+# Get this is to be able to run imshow command via ssh on your rasberry pi when connected to a display
+xhost +
